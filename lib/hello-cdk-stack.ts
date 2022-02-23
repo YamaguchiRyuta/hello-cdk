970c4acce81ee2b0c6e5ef1b58664d74c6274a0b
山口隆太 <steven_siro_vai@hotmail.co.jp>
2022-02-23 18:16:19 +0900

    Add an Amazon S3 bucket

--- lib/hello-cdk-stack.ts
+++ lib/hello-cdk-stack.ts
@@ -1,6 +1,7 @@
 import { Stack, StackProps } from 'aws-cdk-lib';
 import { Construct } from 'constructs';
 // import * as sqs from 'aws-cdk-lib/aws-sqs';
+import { aws_s3 as s3 } from 'aws-cdk-lib';
 
 export class HelloCdkStack extends Stack {
   constructor(scope: Construct, id: string, props?: StackProps) {
@@ -12,5 +13,9 @@ export class HelloCdkStack extends Stack {
     // const queue = new sqs.Queue(this, 'HelloCdkQueue', {
     //   visibilityTimeout: cdk.Duration.seconds(300)
     // });
+
+    new s3.Bucket(this, 'MyFirstBucket', {
+      versioned: true
+    });
   }
 }
